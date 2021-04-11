b0f1072a486035b58d12166eb1e9d4d6f0b5616c
wanglu <wanglu@enginetech.cn>
2021-04-11 16:28:17 +0800

    优化脚本。

--- install.sh
+++ install.sh
@@ -318,13 +318,14 @@ config_caddy_docker_cloudflare(){
 # Install docker and docker compose
 install_docker(){
     echo -e "开始安装 DOCKER "
-    docker version > /dev/null || (curl -fsSL get.docker.com | bash && systemctl enable docker)
+    docker version > /dev/null || curl -fsSL get.docker.com | bash
     echo -e "开始安装 Docker Compose "
     docker-compose version > /dev/null || curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     clear
     echo "启动 Docker "
     service docker start
+    systemctl enable docker
     echo "启动 Docker-Compose "
     docker-compose up -d
     echo
